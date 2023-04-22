9ccb99c2630bcfadb7c0cc5f62ad36957a73346c
yanivagasi1 <58510234+yanivagasi1@users.noreply.github.com>
2023-04-22 20:39:40 +0300

    Add files via upload

---
+++ top-notch-spring-boot-application/src/main/java/com/spring/sample/topnotch/TopNotchApplication.java
@@ -0,0 +1,10 @@
+package com.spring.sample.topnotch;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopNotchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TopNotchApplication.class, args);
+    }
+}
---
+++ top-notch-spring-boot-application/src/test/java/com/spring/sample/topnotch/TopNotchApplicationTests.java
@@ -0,0 +1,12 @@
+package com.spring.sample.topnotch;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopNotchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
